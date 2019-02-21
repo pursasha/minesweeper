94e067ffa86207196718aa0fdc9a95391d562c52
vacaflt <47784895+vacaflt@users.noreply.github.com>
2019-02-21 12:34:06 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,35 +1,38 @@
 import javax.swing.*;
-import java.util.*;
+//import java.util.*;
 import java.awt.*;
 
 public class GUI extends JFrame {
 
     int spacing = 5;
+
     //Creating a window
     public GUI(){
         this.setTitle("Minesweeper"); //sets title to window
-        this.setSize(1286, 800); //sets size of the window
+        this.setSize(450, 530); //sets size of the window
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes sure program is terminated when exited
-        this.setVisible(true); //makes window visible
+        this.setResizable(false); // set it so no one can resize the window
 
         Board board = new Board();
         this.setContentPane(board);//sets content panel
+        this.setVisible(true); //makes window visible
     }
 
     public class Board extends JPanel{
+        
         //sets background color
         public void paintComponent(Graphics graphics){
             graphics.setColor(Color.gray);
-            graphics.fillRect(0, 0,1286,800);
+            graphics.fillRect(0, 0,500,550);
 
             //sets grid
             graphics.setColor(Color.lightGray);
             int i, j;
-            for(i = 0; i < 16; i++) {
+            for(i = 0; i < 10; i++) {
                 for (j = 0; j < 9; j++){
-                    graphics.fillRect(i * 86, j * 86 + (16 * spacing),86-spacing,86-spacing );
-
+                    graphics.fillRect(i * 90, j * 90 + (12 * spacing),90-spacing,90-spacing );
                 }
+
             }
         }
     }
