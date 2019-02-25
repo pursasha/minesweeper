12942f8ca6d45f73daea0a91071758bf2673ce58
CB71 <47640735+CB71@users.noreply.github.com>
2019-02-25 01:23:01 -0500

    Update Tile.java

--- Back_End/Tile.java
+++ Back_End/Tile.java
@@ -3,7 +3,9 @@ import java.lang.Math;
 import java.util.ArrayList;
 
 
-public class Tile implements Serializable {
+public class Tile implements Serializable{
+
+
     private boolean isMine;
     private boolean isRevealed;
     private int x_component;
@@ -13,6 +15,7 @@ public class Tile implements Serializable {
 
 
     public Tile(boolean isMine,boolean isRevealed,int x_component,int y_component){
+
         this.isMine=isMine;
         this.isRevealed=isRevealed;
         this.x_component=x_component;
@@ -21,61 +24,65 @@ public class Tile implements Serializable {
     }
 
 
-
     public boolean isMine() {
         return isMine;
     }
-    public void setIsMine(boolean mine){
+
+
+    public void setIsMine(boolean mine) {
         this.isMine = mine;
     }
 
-    public void setMine(boolean mine) { // Sets mine to on or off based on the passed boolean value (This method is used at times to overwrite the random setMine method below.)
+
+    public void setMine(boolean mine) { // Sets mine to on or off based on the passed boolean value. This method can be used to overwrite the setMine method below.
         isMine = mine;
     }
 
-    public boolean setMine(int board_row_length) { // Randomly sets mine to on or off // Returns True for mine or False for no mine
-        int x, RandomMine,RandomMine2;
-        boolean mine=true;
-        //
-        //
-        int max=1,min=0;
+
+    public boolean setMine(int board_row_length) { // Sets mine to on or off with probability on of 1/n. It returns true for mine or false for no mine.
+
+        int RandomMine,RandomMine2;
+        boolean mine;
         RandomMine = (int) (Math.random() * ((board_row_length - 1) + 1)) + 1;
         RandomMine2= (int) (Math.random() * ((board_row_length - 1) + 1)) + 1;
-        //if(RandomMine==4){
-        //x=(int)(Math.random() * ((max - min) + 1)) + min;
-        //x is in {0,1} based off a Random function
-        //if(x==1){
-            if(RandomMine==RandomMine2){
-            mine = true;}
-        //}
-        //if(x==0){
+
+        if(RandomMine==RandomMine2){
+            mine = true;
+        }
+
             else {
                 mine = false;
-            }
-        //}
+        }
+
         isMine = mine;
         return mine;
     }
+
 
     public boolean isRevealed() {
         return isRevealed;
     }
 
+
     public void setRevealed(boolean revealed) {
         isRevealed = revealed; // Something based off mouse clicking events
     }
+
 
     public int getX_component() {
         return x_component;
     }
 
+
     public void setX_component(int x_component) {
         this.x_component = x_component;
     }
 
+
     public int getY_component() {
         return y_component;
     }
+
 
     public void setY_component(int y_component) {
         this.y_component = y_component;
@@ -86,28 +93,30 @@ public class Tile implements Serializable {
         return neighbors;
     }
 
-    /*public void setNeighbors(ArrayList neighbors) {
-        this.neighbors = neighbors;
-    }*/
 
     public void add_neighbor(Tile new_neighbor_tile){
         this.neighbors.add(new_neighbor_tile);
     }
 
+
     public boolean has_neighbor(Tile possible_neighbor){
+
         if (this.neighbors.contains(possible_neighbor)){
             return true;}
-        else{
-            return false;}
 
+            else{
+            return false;
         }
+    }
+
 
     public int getNumMineNeighbors() {
         return numMineNeighbors;
     }
 
+
     public void setNumMineNeighbors() {
-        int numMineNeigbors=0;
+        int numMineNeighbors=0;
         for(Tile neighbor: this.neighbors) {
             if(neighbor.isMine()){
                 numMineNeighbors++;
@@ -115,5 +124,6 @@ public class Tile implements Serializable {
         }
         this.numMineNeighbors = numMineNeighbors;
     }
-}
 
+
+}
