0f09f287ace22ac8a5c74542af648eb22c428b2f
Ethan Schneider <emanschneider@gmail.com>
2019-02-26 14:31:29 -0500

    combined the frontend board with the backend boards

---
+++ combined/Board.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Board {
+
+    private ArrayList<Tile> board;
+
+    //basic constructor for ease of making a board
+    public Board()
+    {
+      //sets the board to an empty arraylist
+        board = new ArrayList<>();
+    }
+
+    public ArrayList<Tile> getBoard() {
+        return board;
+    }
+
+    public void setSquareBoard(int board_row_length) {
+
+
+        for (int x = 1; x <= board_row_length; x++) {   // This for loops adds all tiles to the list for a square board.
+            for (int y = 1; y <= board_row_length; y++) {
+                Tile newTile = new Tile(true, false, x, y);
+                boolean bool = newTile.setMine(board_row_length); // Places mine on newTile or does not by setMine (with probability of a mine being 1/n)..
+
+                board.add(newTile);
+            }
+        }
+
+        int x1, y1;
+        for (Tile Tile1 : board) {      // Adds neighbors for every tile in a square board
+            x1 = Tile1.getX_component();
+            y1 = Tile1.getY_component();
+            for (Tile Tile2 : board) {
+                int x2, y2;
+                x2 = Tile2.getX_component();
+                y2 = Tile2.getY_component();
+
+                if (((y1 - y2 >= -1) && (y1 - y2 <= 1) && (x1 - x2 >= -1) && (x1 - x2 <= 1)) && (x1 != x2 || y1 != y2)) {
+                    if (!(Tile1.has_neighbor(Tile2))) {
+                        Tile1.add_neighbor(Tile2);
+                        if (!(Tile2.has_neighbor(Tile1))) {
+                            Tile2.add_neighbor(Tile1);
+                        }
+                    }
+                }
+            }
+
+            Tile1.setNumMineNeighbors();
+        }
+    }
+}
---
+++ combined/GUI.java
@@ -0,0 +1,138 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class GUI extends JFrame {
+
+    Board mineBoard = new Board();
+    int height = 532;
+    int width = 450;
+    int boardX, boardY = -99;
+
+    public int mx= -100;
+    public int my= -100;
+
+    //Creating a window
+    public GUI(){
+        mineBoard.setSquareBoard(5);
+        this.setTitle("Minesweeper"); //sets title to window
+        this.setSize(width, height); //sets size of the window
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes sure program is terminated when exited
+        this.setResizable(false); // set it so no one can resize the window
+
+        FBoard GUIboard = new FBoard();
+        this.setContentPane(GUIboard);//sets content panel
+        this.setVisible(true); //makes window visible
+
+        Move move= new Move();
+        this.addMouseMotionListener(move);
+
+        Click click= new Click();
+        this.addMouseListener(click);
+
+
+
+    //    tile.add_neighbor(new Tile(false, true, 2, 2));
+
+
+
+
+    }
+
+    public class FBoard extends JPanel{
+
+        //sets background color
+        public void paintComponent(Graphics graphics){
+            graphics.setColor(Color.gray);
+            graphics.fillRect(0, 0,width,height);
+
+            //sets grid
+            int spacing = 5;
+            int i, j;
+
+            for(i = 0; i < 5; i++) {
+                for (j = 0; j < 5; j++){
+                    graphics.setColor(Color.lightGray);
+
+                    if(mx>=spacing+i * 86 && mx< spacing+i * 86+86-spacing && my>=j * 86+86 + spacing+26 && my< j * 86+86  +26 +spacing +86-spacing )
+                    {
+                        graphics.setColor(Color.blue);
+                        //this gets the corresponding tile coordinates
+                        boardX=i;
+                        boardY=j;
+                        //colors tile red it it is a mine
+                        if (mineBoard.getBoard().get(boardX+boardY).getMine())
+                        {
+                          graphics.setColor(Color.red);
+                        }
+                    }
+                    graphics.fillRect(i * 90, j * 90 + (12 * spacing),90-spacing,90-spacing );
+
+                }
+            }
+        }
+    }
+    //sets display for mouse movement and coordinates
+    public class Move implements MouseMotionListener
+    {
+
+        public void mouseDragged(MouseEvent a)
+        {
+
+        }
+        public void mouseMoved(MouseEvent a)
+        {
+            System.out.println("the mouse was moved");
+            mx=a.getX();
+            my=a.getY();
+            System.out.println("x:"+ mx +" y:"+my);
+
+            System.out.println("board X:"+boardX+" board y:"+boardY);
+        }
+
+    }
+    //sets display for mouse click and coordinates
+    public class Click implements MouseListener {
+        public void mouseClicked(MouseEvent a) {
+            System.out.println("the mouse was clicked");
+
+            //checks if the clicked tile is a mine
+            if (mineBoard.getBoard().get(boardX+boardY).getMine())
+            {
+              System.out.println("the tile was a mine!");
+            }
+            //checks if clicked tile has mine neighbors
+            else if (mineBoard.getBoard().get(boardX+boardY).getNumMineNeighbors()>0)
+            {
+              System.out.println("there are " + mineBoard.getBoard().get(boardX+boardY).getNumMineNeighbors() + " mine neighbors");
+            }
+            else
+            {
+              System.out.println("there are no mine neighbors!");
+            }
+
+
+
+        }
+
+        public void mouseEntered(MouseEvent a) {
+
+        }
+
+        public void mouseExited(MouseEvent a) {
+
+        }
+
+        public void mousePressed(MouseEvent a) {
+
+        }
+
+        public void mouseReleased(MouseEvent a) {
+
+        }
+
+    }
+
+}
