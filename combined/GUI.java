b66fb0d383b45805e53e0b8870801cb72a42656f
vacaflt <47784895+vacaflt@users.noreply.github.com>
2019-02-27 17:40:57 -0500

    Update GUI.java

--- combined/GUI.java
+++ combined/GUI.java
@@ -1,24 +1,24 @@
 import javax.swing.*;
-import java.util.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 
 public class GUI extends JFrame {
 
-    int boardSize = 8;
-
+    public int mx = -100;
+    public int my = -100;
     Board mineBoard = new Board();
-    int height = (532/5)*boardSize;
-    int width = (450/5)*boardSize;
-    int boardX, boardY, tileID = 0;
+    int height = 532;
+    int width = 450;
+    int boardX, boardY = -99;
 
-    public int mx= -100;
-    public int my= -100;
+    boolean gameOver, tile1;
 
     //Creating a window
-    public GUI(){
-        mineBoard.setSquareBoard(boardSize);
+    public GUI() {
+        mineBoard.setSquareBoard(5);
         this.setTitle("Minesweeper"); //sets title to window
         this.setSize(width, height); //sets size of the window
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes sure program is terminated when exited
@@ -28,102 +28,99 @@ public class GUI extends JFrame {
         this.setContentPane(GUIboard);//sets content panel
         this.setVisible(true); //makes window visible
 
-        Move move= new Move();
+        Move move = new Move();
         this.addMouseMotionListener(move);
 
-        Click click= new Click();
+        Click click = new Click();
         this.addMouseListener(click);
+
 
     }
 
-    public class FBoard extends JPanel{
+    public class FBoard extends JPanel {
 
         //sets background color
-        public void paintComponent(Graphics graphics){
+        public void paintComponent(Graphics graphics) {
             graphics.setColor(Color.gray);
-            graphics.fillRect(0, 0,width,height);
+            graphics.fillRect(0, 0, width, height);
+
 
             //sets grid
             int spacing = 5;
             int i, j;
 
-            for(i = 0; i < boardSize; i++) {
-                for (j = 0; j < boardSize; j++){
+            for (i = 0; i < 5; i++) {
+                for (j = 0; j < 5; j++) {
+
                     graphics.setColor(Color.lightGray);
 
-                    //testing the mine layout
-                    if (mineBoard.getBoard().get(i+(boardSize*j)).getMine())
-                    {
-                      graphics.setColor(Color.pink);
+                    //sets screen black if mine is clicked
+                    if (gameOver == true) {
+                        graphics.setColor(Color.black);
+                        graphics.fillRect(0, 0, width, height);
                     }
 
-                    if(mx>=spacing+i * 86 && mx< spacing+i * 86+86-spacing && my>=j * 86+86 + spacing+26 && my< j * 86+86  +26 +spacing +86-spacing )
-                    {
+
+                    if (mx >= spacing + i * 86 && mx < spacing + i * 86 + 86 - spacing && my >= j * 86 + 86 + spacing + 26 && my < j * 86 + 86 + 26 + spacing + 86 - spacing) {
+
+                        //  graphics.setColor(Color.blue);
                         //this gets the corresponding tile coordinates
-                        boardX=i;
-                        boardY=j;
-                        //grabs specific tile ID for the tile
-                        tileID= boardX + (boardSize*boardY);
+                        boardX = i;
+                        boardY = j;
                         //colors tile red it it is a mine
-                        if (mineBoard.getBoard().get(tileID).getMine())
-                        {
-                          graphics.setColor(Color.red);
-                        }
-                        else
-                        {
-                          graphics.setColor(Color.blue);
+                        if (mineBoard.getBoard().get(boardX + boardY).getMine()) {
+                            graphics.setColor(Color.red);
+                        } else {
+                            if (mineBoard.getBoard().get(boardX + boardY).isRevealed()) {
+                                graphics.setColor(Color.white);
+                            } else graphics.setColor(Color.blue);
                         }
                     }
-                    graphics.fillRect(i * 90, j * 90 + (12 * spacing),90-spacing,90-spacing );
-
+                    graphics.fillRect(i * 90, j * 90 + (12 * spacing), 90 - spacing, 90 - spacing);
                 }
             }
         }
     }
-    //sets display for mouse movement and coordinates
-    public class Move implements MouseMotionListener
-    {
 
-        public void mouseDragged(MouseEvent a)
-        {
+    //sets display for mouse movement and coordinates
+    public class Move implements MouseMotionListener {
+
+        public void mouseDragged(MouseEvent a) {
 
         }
-        public void mouseMoved(MouseEvent a)
-        {
-            System.out.println("the mouse was moved");
-            mx=a.getX();
-            my=a.getY();
-            System.out.println("x:"+ mx +" y:"+my);
 
-            System.out.println("board X:"+boardX+" board y:"+boardY);
+        public void mouseMoved(MouseEvent a) {
+            System.out.println("the mouse was moved");
+            mx = a.getX();
+            my = a.getY();
+            System.out.println("x:" + mx + " y:" + my);
+
+            System.out.println("board X:" + boardX + " board y:" + boardY);
         }
 
     }
+
     //sets display for mouse click and coordinates
     public class Click implements MouseListener {
         public void mouseClicked(MouseEvent a) {
             System.out.println("the mouse was clicked");
-            try {
-              //checks if the clicked tile is a mine
-              if (mineBoard.getBoard().get(tileID).getMine())
-              {
-                System.out.println("the tile was a mine!");
-              }
-              //checks if clicked tile has mine neighbors
-              else if (mineBoard.getBoard().get(tileID).getNumMineNeighbors()>0)
-              {
-                System.out.println("there are " + mineBoard.getBoard().get(tileID).getNumMineNeighbors() + " mine neighbors");
-              }
-              else
-              {
-                System.out.println("there are no mine neighbors!");
-              }
-            }
-            catch(Exception e)
-            {
-              System.out.println("");
-            }
+            mineBoard.getBoard().get(boardX + boardY).setRevealed(true);
 
+
+            //checks if the clicked tile is a mine
+            if (mineBoard.getBoard().get(boardX + boardY).getMine()) {
+                System.out.println("the tile was a mine!");
+                gameOver = true;
+
+            }
+            //checks if clicked tile has mine neighbors
+            else if (mineBoard.getBoard().get(boardX + boardY).getNumMineNeighbors() > 0) {
+                System.out.println("there are " + mineBoard.getBoard().get(boardX + boardY).getNumMineNeighbors() + " mine neighbors");
+                
+            } else {
+                System.out.println("there are no mine neighbors!");
+
+            }
 
 
         }
