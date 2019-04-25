d8de2cf9878b17ba943fa3912a1ff9d727781306
vacaflt <47784895+vacaflt@users.noreply.github.com>
2019-04-25 16:34:23 -0400

    Update Minesweeper.java

--- combined/Minesweeper.java
+++ combined/Minesweeper.java
@@ -6,29 +6,29 @@ public class Minesweeper implements Runnable {
 
         //loads splash and menu
         Menu menu = new Menu();
-        menu.showFrame();
+        menu.showSplash();
     }
 
     public Minesweeper(int boardSize, int mine_probability, int mineRadius,int gameMode) {
         this.boardSize = boardSize;
         gui = new GUI(boardSize, mine_probability, mineRadius, gameMode);
     }
-	public Minesweeper(int difficulty, int mineRadius, int gameMode) {
-		switch (difficulty)
-		{
-			case 0:
-				boardSize = 10;
-			break;
-			case 1:
-				boardSize = 20;
-			break;
-			case 2:
-				boardSize = 30;
-			break;
-			default:
-				boardSize = 20;
-			break;
-		}
+    public Minesweeper(int difficulty, int mineRadius, int gameMode) {
+        switch (difficulty)
+        {
+            case 0:
+                boardSize = 10;
+                break;
+            case 1:
+                boardSize = 20;
+                break;
+            case 2:
+                boardSize = 30;
+                break;
+            default:
+                boardSize = 20;
+                break;
+        }
         gui = new GUI(difficulty, gameMode, mineRadius);
     }
 
@@ -37,13 +37,13 @@ public class Minesweeper implements Runnable {
         while(!gui.restart){
             gui.repaint();
         }
-		try
-		{
-			Thread.sleep(2000);
-		}
-		catch (InterruptedException e)
-		{
-		}
-		gui.restart();
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+        }
+        gui.restart();
     }
 }
