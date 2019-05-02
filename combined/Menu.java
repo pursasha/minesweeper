b5471a16445f379a67020f94697a85f425059147
vacaflt <47784895+vacaflt@users.noreply.github.com>
2019-05-01 21:05:43 -0400

    Update Menu.java

--- combined/Menu.java
+++ combined/Menu.java
@@ -97,7 +97,16 @@ public class Menu extends JFrame implements ActionListener, MouseListener, Mouse
             System.exit(0);
         }else if (cmd.equals("Instructions")) {
             System.out.println("Help -> Instructions");
-            //instructions here
+            try
+                {
+                    Desktop gamePlay=Desktop.getDesktop();
+                    gamePlay.browse(new URI("https://web.cs.sunyit.edu/~schneieh/images/Game%20play%20instructions.pdf"));
+
+                }
+                catch(Exception ex)
+                {
+                    ex.printStackTrace();
+                }
 
         }else if (cmd.equals("Credits")) {
             System.out.println("About -> Credits");
